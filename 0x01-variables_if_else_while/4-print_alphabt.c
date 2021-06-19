3fb94c05eba1202ba6dac8b0c2dddd5ee0149e2f
angelikrussi <hannamrrussi@gmail.com>
2021-06-18 22:38:55 -0500

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+/**
+ * main - this  program  print the alphabet in lowercase
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char n;
+for (n = 'a'n <= 'z'; ++n)
+{
+if (n != q && n != e)
+}
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
