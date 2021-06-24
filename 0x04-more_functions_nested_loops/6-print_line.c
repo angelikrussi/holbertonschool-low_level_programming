a3c521d865d7b4539327b4199ff5223573ee3ffd
angelikrussi <hannamrrussi@gmail.com>
2021-06-24 14:07:22 -0500

    draw a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ *print_line - draw a line.
+ *@n:line number
+ */
+void print_line(int n)
+{
+	int m;
+
+	for (m = 0; m < n; m++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
