e182b44f380823281bf70934ab27dea39520301a
angelikrussi <hannamrrussi@gmail.com>
2021-07-02 11:35:31 -0500

    function strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,30 +9,23 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int a, b;
+	int a;
 
-	for (a = 0; s1[a] != '\0'; a++)
+	for (a = 0; (s1[a] != '\0' && s2[a] != '\0') && s1[a] == s2[a]; a++)
 	{
-		;
+	}
+	if (s1[a] < s2[a])
+	{
+		return (s1[a] - s2[a]);
 	}
 
-	for (b = 0; s2[b] != '\0'; b++)
+	else if (s1[a] > s2[a])
 	{
-		if (s1[a] == s2[b])
-		{
-			return (0);
-		}
+		return (s1[a] - s2[a]);
+	}
 
-		else if (s1[a] > s2[b])
-
-		{
-			return (s1[a] - s2[b]);
-		}
-
-		else
-
-		{
-			return (s1[a] + s2[b]);
-		}
+	else
+	{
+		return (s1[a] + s2[a]);
 	}
 }
