dd187fb935ce6acbfa60b09a159c67707109a1d6
angelikrussi <hannamrrussi@gmail.com>
2021-07-05 20:03:18 -0500

    function strstr

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _strpbrk - this function that gets the length of a prefix substring.
+ * @s: This is the main C string to be scanned.
+ * @accept: This is the small string to be searched with-in haystack string.
+ * Return: 0.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		int b = 0;
+		while (accept[b] != '\0')
+		{
+			if (s[a] == accept[b])
+			{
+				return (s + a);
+			}
+			b++;
+		}
+	}
+	return (0);
+}
