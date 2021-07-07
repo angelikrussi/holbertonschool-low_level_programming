69a5726c0fb978994203109790ab809a99ca8d64
angelikrussi <hannamrrussi@gmail.com>
2021-07-06 20:47:24 -0500

    function strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _strstr- function that locates a substring.
+ * @haystack:The string to be searched.
+ * @needle:The string that we need to search in string.
+ * Return: 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		if (*haystack == *needle)
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+	if (*haystack == *needle)
+		return (haystack);
+	else
+		return (0);
+}
