7a844cc07ea8178a7e5f20395e210af9a631d21b
angelikrussi <hannamrrussi@gmail.com>
2021-07-08 14:21:02 -0500

    prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints from the first occurrence of a char.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: o.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
