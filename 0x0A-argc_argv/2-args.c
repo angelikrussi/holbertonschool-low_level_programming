c2ba5d54273331fa6708dc82773a7ad3c6800bf4
angelikrussi <hannamrrussi@gmail.com>
2021-07-08 17:42:10 -0500

    args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - program that prints all arguments it receives.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: o.
+ */
+int main(int argc, char *argv[])
+{
+	int w = 0;
+	while (w < argc)
+	{
+		printf("%s\n", argv[w]);
+		w++;
+	}
+	return (0);
+}
