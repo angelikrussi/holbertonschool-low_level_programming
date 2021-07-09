991eacbba1c7534856939075d2920ad5f19be0a3
angelikrussi <hannamrrussi@gmail.com>
2021-07-09 17:28:35 -0500

    function pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion-function that returns the value of x raised to the power of y.
+ *
+ * Return:  0.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	if (y < 0)
+	{
+		return(-1);
+	}
+	return(_pow_recursion(x , y - 1) * x);
+}
