a3e5891a46a99b1932263f536a1d101aa01848eb
angelikrussi <hannamrrussi@gmail.com>
2021-07-10 00:03:50 -0500

    function wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * wildcmp -  this unction that compares two strings and returns 1 
+ *@s: variable.
+ * Return: Always 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	;
+}
