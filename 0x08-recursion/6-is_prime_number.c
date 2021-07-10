55ff1c9b2609f403990d3da179bb14231fd9ff39
angelikrussi <hannamrrussi@gmail.com>
2021-07-09 22:57:35 -0500

    Function prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number.
+ *@n: variable.
+ * Return: Always 0.
+ */
+int is_prime_number(int n)
+{
+	;
+}
