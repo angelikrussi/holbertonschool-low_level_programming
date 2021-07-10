688265eb054cfe90002cdaecf6b21ec42834c18b
angelikrussi <hannamrrussi@gmail.com>
2021-07-09 23:02:52 -0500

    function palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * is_palindrome -  that returns 1 if a string is a palindrome and 0 if not.
+ *@s: variable.
+ * Return: Always 0.
+ */
+int is_palindrome(char *s)
+{
+	;
+}
