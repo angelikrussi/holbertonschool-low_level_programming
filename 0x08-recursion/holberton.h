6f76b3234a3d4c7f58a7e5bdd36dc3e7558d9415
angelikrussi <hannamrrussi@gmail.com>
2021-07-12 19:38:37 -0500

    Header file

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,7 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-int _putchar(char c);
+void _putchar(int);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -11,5 +11,6 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int prime_help(int n);
 
 #endif /* HOLBERTON_H */
