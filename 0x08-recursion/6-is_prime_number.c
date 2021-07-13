02e6e2be70c70688d16ab04126dcf61db979fb9f
angelikrussi <hannamrrussi@gmail.com>
2021-07-12 19:39:56 -0500

    function prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,11 +1,50 @@
 #include "holberton.h"
+#include "stdio.h"
+/**
+ *is_prime_number - func that returns 1 if a num is a prime num, 0 otherwise
+ *@n: variable
+ *Return: zero or one
+ */
+
+int is_prime_number(int n)
+
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+
+	if (n == 2 || n == 3 || n == 5 || n == 7)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (prime_help(n));
+	}
+
+	return (0);
+}
 
 /**
- * is_prime_number - returns 1 if the input integer is a prime number.
- *@n: variable.
- * Return: Always 0.
+ *prime_help - returns 1 if the input integer is a prime number.
+ *@n: variable
+ *Return: zero or one
  */
-int is_prime_number(int n)
+
+int prime_help(int n)
 {
-	;
+
+	if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n == 7)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (1);
+	}
+	return (0);
+
 }
