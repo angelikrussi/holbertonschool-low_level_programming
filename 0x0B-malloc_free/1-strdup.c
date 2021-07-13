0857aca6eb3e409fb0defe44cbd29115fe8f6d30
angelikrussi <hannamrrussi@gmail.com>
2021-07-13 18:57:00 -0500

    function strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * *_strdup - check the code for Holberton School students.
+ * @str: The string that you want to copy.
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+	char *array;
+	unsigned int size = 0;
+
+	if (str)
+	{
+		while (str[size++])
+			;
+
+		array = malloc(sizeof(char) * size);
+		if (array)
+		{
+			while (size--)
+				array[size] = str[size];
+
+			return (array);
+		}
+	}
+	return ('\0');
+}
