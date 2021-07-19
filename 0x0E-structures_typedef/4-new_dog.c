7e468d8c60a15f0207babb10fcda90a4c5cc8da9
angelikrussi <hannamrrussi@gmail.com>
2021-07-19 18:45:35 -0500

    creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,10 +12,10 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-
-	dog = malloc(sizeof(struct dog));
+	/* reserving memory */
+	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
-	return (NULL);
+		return (NULL);
 
 	/* Cpunting name pointer*/
 	if (name == NULL)
