12e7f7f07d336c51c8fad881a2bdd80994ebdb7f
angelikrussi <hannamrrussi@gmail.com>
2021-07-19 12:41:20 -0500

    header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,14 +12,6 @@ struct dog
 	char *name;
 	float age;
 	char owner;
-};
-/* Additional functions*/
-typedef struct dog dog_t;
-
-int _putchar(char);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+}
 
 # endif /* DOG_H*/
