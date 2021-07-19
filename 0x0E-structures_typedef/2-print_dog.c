ba56500ad2bc6e703c70a567d1232bab8124f9a9
angelikrussi <hannamrrussi@gmail.com>
2021-07-19 17:00:08 -0500

    function init_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,16 +10,11 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if (d->name != NULL)
-			printf("Name: %s\n", d->name);
-		else
-			printf("nil");
+		if (d->name == NULL)
+			d->name = "(nil)";
+		if (d->owner == NULL)
+			d->owner = "(nil)";
 
-		printf("Age: %f\n", d->age);
-
-		if (d->owner != NULL)
-			printf("Owner: %s\n", d->owner);
-		else
-			printf("nil");
+		printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
 	}
 }
