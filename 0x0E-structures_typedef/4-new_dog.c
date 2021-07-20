3b5c52c94b210051df70d117fc85f7f352763b1f
angelikrussi <hannamrrussi@gmail.com>
2021-07-19 19:03:44 -0500

    creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,30 +7,43 @@
  *@name: dog name.
  *@age: dog's age.
  *@owner: owner's name.
- * Return: dog.
+ * Return:new_dog.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p;
-	/* reserving memory to struct*/
-	p = malloc(sizeof(dog_t));
-	if (p == NULL)
+
+	int i, n, o;
+	struct dog *new_dog;
+
+	dog_t *new_dog = malloc(sizeof(dog_t));
+
+	for (n = 0; name[n]; n++)
+		;
+	for (o = 0; owner[o]; o++)
+		;
+	if (new_dog == NULL)
 		return (NULL);
-	/* Cpunting name pointer*/
-	if (name == NULL)
+
+	new_dog->name = malloc(sizeof(char) * (n + 1));
+	if (new_dog->name == NULL)
 	{
-		free(p);
-		free(owner);
+		free(new_dog);
 		return (NULL);
 	}
-	if (owner == NULL)
+
+	new_dog->owner = malloc(sizeof(char) * (o + 1));
+	if (new_dog->owner == NULL)
 	{
-		free(p);
-		free(name);
+		free(new_dog->name);
+		free(new_dog);
 		return (NULL);
 	}
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
-	return (p);
+	for (i = 0; i <= n; i++)
+		new_dog->name[i] = name[i];
+	new_dog->age = age;
+
+	for (i = 0; i <= o; i++)
+		new_dog->owner[i] = owner[i];
+
+	return (new_dog);
 }
