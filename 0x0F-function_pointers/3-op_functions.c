68869b4f0b02e0e4af3080e6d21ab3f1efadeba4
angelikrussi <hannamrrussi@gmail.com>
2021-07-21 21:46:18 -0500

    Todo

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+/**
+ * struct op - addition.
+ *
+ * @op: number
+ * @f: number
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * struct op -  subtraction.
+ *
+ * @op: number
+ * @f: number
+ */
+int op_add(inint op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * struct op - multiplication
+ *
+ * @op:number
+ * @f:number
+ */
+int op_mul(int a, int b)
+{
+   return (a * b);
+}
+
+/**
+ * struct op - division
+ *
+ * @op: number
+ * @f: number
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo
+ *
+ * @a: number
+ * @b: number
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
