4a2d2d667026329b0b168a641e2e43b3e87a18de
angelikrussi <hannamrrussi@gmail.com>
2021-07-23 23:36:42 -0500

    header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,17 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define   VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+
+/**
+ * struct print -  operations
+ * @tp: operator
+ * @function: print function
+ */
+typedef struct print
+{
+	char *tp;
+	void (*function)(va_list);
+} prnt_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
