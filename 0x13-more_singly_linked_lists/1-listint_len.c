429d871d9fb09a615f897b6f7043607ccd7bf6b8
root <root@LAPTOP-H35T4RJT.localdomain>
2021-08-03 18:08:46 -0500

    function listint len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * listint_len -  this function prints all the elements of a listint_t list.
+ * @h: linked list pointer.
+ * Return: Always 0.
+ */
+size_t listint_len(const listint_t *h)
+{
+    size_t counter;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	for (counter = 0; h != NULL; counter++)
+	{
+        h = h->next;
+	}
+	return (counter);
+}
