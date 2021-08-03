c6906ef5451c1cc9702c8139132e556738b7abae
root <root@LAPTOP-H35T4RJT.localdomain>
2021-08-03 18:32:21 -0500

    function listint len

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -6,7 +6,7 @@
  */
 size_t listint_len(const listint_t *h)
 {
-    size_t counter;
+	size_t counter;
 
 	if (h == NULL)
 	{
@@ -15,7 +15,7 @@ size_t listint_len(const listint_t *h)
 
 	for (counter = 0; h != NULL; counter++)
 	{
-        h = h->next;
+		h = h->next;
 	}
 	return (counter);
 }
