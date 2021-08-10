905b49bcfecda5db7a11048673be892b2464c06b
angelikrussi <hannamrrussi@gmail.com>
2021-08-09 23:13:18 -0500

    Creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * create_file - Creates a file.
+ * @filename: Pointer.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: (-1)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, counter = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (counter = 0; text_content[counter];)
+			counter++;
+	}
+	/*O_CREAT= Create the file if it does not exist.*/
+	/*O_RDWR= Open for read and write.*/
+	/*O_TRUNC= Write, starting at the beginning of the file.*/
+	/*600= permission to read and write to a file for the owner*/
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	w = write(o, text_content, counter);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
+}
